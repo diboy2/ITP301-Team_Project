dbb328585bdac7632f30cc562f8615398c7e6f16
Adrian <cagaanan@usc.edu>
2015-04-27 20:42:21 -0700

    Made changes

--- main.js
+++ main.js
@@ -31,3 +31,46 @@ $('.selector button').click(function(e) {
 setTimeout(function() { toggleOptions('.selector'); }, 100);
 
 
+$(document).ready(function(){
+   animateBird();
+    
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 100;
+    var w = $(window).width() - 100;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateBird(){
+    var newq = makeNewPosition();
+    var oldq = $('.bird').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('.bird').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateBird();        
+    });
+    
+};
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
