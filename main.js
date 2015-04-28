fadadf4fa1b0addbc34e9ab042ef83cca2362188
Adrian <cagaanan@usc.edu>
2015-04-27 21:27:01 -0700

    Changed

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var nbOptions = 8;
 var angleStart = -360;
 var birds = [];
+
+
 // jquery rotate animation
 function rotate(li,d) {
     $({d:angleStart}).animate({d:d}, {
@@ -29,11 +31,31 @@ $('.selector button').click(function(e) {
 });
 
 setTimeout(function() { toggleOptions('.selector'); }, 100);
-// function addBird(){
-//     var bird = $('<img src="images/bird.png" alt="Smiley face" height="80" width="80">');
-//     $(body).append(bird);
-// }
+function addBird(birdClass,imageSRC){
 
+    var bird = $('<img class="bird-food'+birdClass+'"  src="images/'+imageSRC+'"  alt="Smiley face" height="80" width="80">');
+    var yP = Math.random() *  $(window).height();
+    var xP = Math.random() *  $(window).width();
+    
+    // var bird={
+    //     id
+    //     yP:yP
+    //     xP:xP
+    // }   
+    bird.css({top:yP, left: xP, position:'absolute'});
+    $("body").append(bird);
+
+    
+
+
+}
+
+$(document).ready(function(){
+   animateBird();
+   $("#c1").on("click",function(){
+        addBird("1","bird.png");
+   });
+});
 
 
 function checkIntersection(){
